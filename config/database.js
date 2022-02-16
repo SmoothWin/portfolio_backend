aba8a6cabe30a9af7084f6eeb44123472c11dc36
Christian Chitanu <christian.chitanu@hotmail.com>
2022-02-16 15:15:41 -0500

    changed a few things regarding strapi functionalities

--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
-
-const path = require('path');
-
 module.exports = ({ env }) => ({
-  connections: {
-    default:{
-      client: "postgres",
-            host: env("DATABASE_HOST", "localhost"),
-            port: env.int("DATABASE_PORT", 5432),
-            database: env("DATABASE_NAME", "bank"),
-            username: env("DATABASE_USERNAME", "postgres"),
-            password: env("DATABASE_PASSWORD", "0000"),
-            schema: env("DATABASE_SCHEMA", "public"),
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
   },
 });
